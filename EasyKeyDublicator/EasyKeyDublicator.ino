de12cfa44e784a6d8e27062f189ce60525defa37
AlexMalov <47302079+AlexMalov@users.noreply.github.com>
2019-02-10 18:29:51 +0300

    Add files via upload

--- EasyKeyDublicator/EasyKeyDublicator.ino
+++ EasyKeyDublicator/EasyKeyDublicator.ino
@@ -1,26 +1,26 @@
 #include <OneWire.h>
-#include "musical_notes.h"
+#include "pitches.h"
 #include "analogComp.h"
 
 #define iButtonPin A5      // Линия data ibutton
+#define R_Led 2            // RGB Led
+#define G_Led 3
+#define B_Led 4
 #define ACpinGnd 5         // Земля аналогового компаратора Cyfral
 #define ACpin 6            // Вход аналогового компаратора 3В для Cyfral
 #define BtnPin 7           // Кнопка переключения режима чтение/запись
 #define BtnPinGnd 8        // Земля кнопки переключения режима чтение/запись
 #define speakerPin 9       // Спикер, он же buzzer, он же beeper
 #define speakerPinGnd 10    // Спикер, он же buzzer, он же beeper
-#define R_Led 2
-#define G_Led 3
-#define B_Led 4
+
 
 OneWire ibutton (iButtonPin); 
-
 byte addr[8];
 byte ReadID[8];                           // массив с данными для записи
 bool readflag = false;                    // флаг сигнализируе, что данные с ключа успечно прочианы в ардуино
 bool writeflag = false;                   // режим запись/чтение
 bool preBtnPinSt = HIGH;
-enum emkeyType {unknown, dallas, cifral, metacom};    // тип оригинального ключа  
+enum emkeyType {unknown, dallas, cyfral, metacom};    // тип оригинального ключа  
 emkeyType keyType;
 
 void setup() {
@@ -29,15 +29,11 @@ void setup() {
   pinMode(speakerPin, OUTPUT);
   pinMode(speakerPinGnd, OUTPUT); digitalWrite(speakerPinGnd, LOW); // подключаем второй пин спикера к земле
   pinMode(ACpin, INPUT);                                            // Вход аналогового компаратора 3В для Cyfral
-  pinMode(ACpinGnd, OUTPUT); digitalWrite(ACpinGnd, LOW);           // подключаем второй пин аналогового компаратора Cyfral к земле
-  
-  pinMode(R_Led, OUTPUT);
-  pinMode(G_Led, OUTPUT);
-  pinMode(B_Led, OUTPUT);
+  pinMode(ACpinGnd, OUTPUT); digitalWrite(ACpinGnd, LOW);           // подключаем второй пин аналогового компаратора Cyfral к земле 
+  pinMode(R_Led, OUTPUT); pinMode(G_Led, OUTPUT); pinMode(B_Led, OUTPUT);  //RGB-led
   clearLed();
   digitalWrite(B_Led, HIGH);                                //awaiting of origin key data
   Serial.begin(115200);
-  analogComparator.setOff();
   Sd_StartOK();
 }
 
@@ -48,7 +44,7 @@ void clearLed(){
 }
 
 byte getRWtype(){    
-  // 0 это неизвестный тип болванки, нужно перебирать алгоритмы записи
+  // 0 это неизвестный тип болванки, нужно перебирать алгоритмы записи (TM-01)
   // 1 это RW-1990, RW-1990.1, ТМ-08, ТМ-08v2
   // 2 это RW-1990.2
   ibutton.reset(); ibutton.write(0xD1); // проуем снять флаг записи для RW-1990.1
@@ -75,76 +71,49 @@ byte getRWtype(){
     return 2; // это RW-1990.2
   }
   Serial.println("Type: Unknown! ");
-  return 0;                              // это неизвестный тип DS1990
+  return 0;                              // это неизвестный тип DS1990, нужно перебирать алгоритмы записи (TM-01)
 }
 
-bool write2iBtnRW1990_1(){
-  if (!ibutton.reset()) return false;
-  ibutton.write(0xD1);                    // send 0xD1 - флаг записи
-  ibutton.write_bit(0);                   // записываем значение флага записи = 0 - разрешить запись
+bool write2iBtnRW1990_1_2_TM01(byte rwType){              // функция записи на RW1990.1
+  byte rwCmd, rwFlag = 1;
+  switch (rwType){
+    case 0: rwCmd = 0xC1; break;              //TM-01C(F)
+    case 1: rwCmd = 0xD1; rwFlag = 0; break;  // RW1990.1  флаг записи инвертирован
+    case 2: rwCmd = 0x1D; break;              //RW1990.2
+  }
+  ibutton.reset(); ibutton.write(rwCmd);       // send 0xD1 - флаг записи
+  ibutton.write_bit(rwFlag);                   // записываем значение флага записи = 1 - разрешить запись
   delay(10); pinMode(iButtonPin, INPUT);
-  ibutton.reset(); ibutton.write(0xD5);   // команда на запись
+  ibutton.reset(); ibutton.write(0xD5);        // команда на запись
   for (byte i = 0; i<8; i++){
     digitalWrite(R_Led, !digitalRead(R_Led));
-    BurnByte(~ReadID[i]);                 // запись происходит инверсно 
-    Serial.print('*');
-    Sd_WriteStep();
-  } 
-  ibutton.write(0xD1);    // send 0xD1 - флаг записи
-  ibutton.write_bit(1);   // записываем значение флага записи = 1 - отключаем запись
-  delay(10); pinMode(iButtonPin, INPUT);
-  return true;
-}
-
-bool write2iBtnRW1990_2(){
-  if (!ibutton.reset()) return false;
-  ibutton.write(0x1D);                    // send 0x1D - флаг записи
-  ibutton.write_bit(1);                   // записываем значение флага записи = 1
-  delay(10); pinMode(iButtonPin, INPUT);
-  ibutton.reset(); ibutton.write(0xD5);   // команда на запись
-  for (byte i = 0; i<8; i++){
-    digitalWrite(R_Led, !digitalRead(R_Led));
-    BurnByte(ReadID[i]);
-    Serial.print('*');
-    Sd_WriteStep();
-  } 
-  ibutton.write(0x1D);                    // send 0xD1 - флаг записи
-  ibutton.write_bit(0);                   // записываем значение флага записи = 0 - отключаем запись
-  delay(10); pinMode(iButtonPin, INPUT);
-  return true;
-}
-
-bool write2iBtnTM01C(){
-  if (!ibutton.reset()) return false;
-  ibutton.write(0xC1);                    // send 0xC1 - флаг записи
-  ibutton.write_bit(1);                   // записываем значение флага записи = 1 - разрешить запись
-  delay(5); pinMode(iButtonPin, INPUT);
-  ibutton.reset(); ibutton.write(0xC5);   // команда на запись
-  for (byte i = 0; i<8; i++){
-    digitalWrite(R_Led, !digitalRead(R_Led));
-    if ((keyType == metacom)||(keyType == cifral)) BurnByte(~ReadID[i]); // для ключей metacom и cifral пишем только 4 байта инверсно
+    if (rwType == 1) BurnByte(~ReadID[i]);      // запись происходит инверсно 
       else BurnByte(ReadID[i]);
     Serial.print('*');
     Sd_WriteStep();
   } 
-  ibutton.write(0xC1);    // send 0xD1 - флаг записи
-  ibutton.write_bit(0);   // записываем значение флага записи = 0 - отключаем запись
-  delay(5); pinMode(iButtonPin, INPUT);
+  ibutton.write(rwCmd);                     // send 0xD1 - флаг записи
+  ibutton.write_bit(!rwFlag);               // записываем значение флага записи = 1 - отключаем запись
+  delay(10); pinMode(iButtonPin, INPUT);
+  digitalWrite(R_Led, LOW);       
+  if (!dataIsBurningOK()){          // проверяем корректность записи
+    Serial.println(" The key copy faild");
+    Sd_ErrorBeep();
+    digitalWrite(R_Led, HIGH);
+    return false;
+  }
+  Serial.println(" The key has copied successesfully");
+  if ((keyType == metacom)||(keyType == cyfral)){      //переводим ключ из формата dallas
+    ibutton.reset();
+    if (keyType == cyfral) ibutton.write(0xCA);       // send 0xCA - флаг финализации Cyfral
+      else ibutton.write(0xCB);                       // send 0xCA - флаг финализации metacom
+    ibutton.write_bit(1);                             // записываем значение флага финализации = 1 - перевезти формат
+    delay(10); pinMode(iButtonPin, INPUT);
+  }
+  Sd_ReadOK();
+  delay(500);
+  digitalWrite(R_Led, HIGH);
   return true;
-}
-
-bool finalizationTM01cCifral(){
-  if (!ibutton.reset()) return false;
-  ibutton.write(0xCA);                    // send 0xCA - флаг финализации
-  ibutton.write_bit(1);                   // записываем значение флага записи = 1 - разрешить запись
-  delay(10); pinMode(iButtonPin, INPUT);
-}
-
-bool finalizationTM01cMetacom(){
-  if (!ibutton.reset()) return false;
-  ibutton.write(0xCB);                    // send 0xCA - флаг финализации
-  ibutton.write_bit(1);                   // записываем значение флага записи = 1 - разрешить запись
-  delay(10); pinMode(iButtonPin, INPUT);
 }
 
 bool dataIsBurningOK(){
@@ -175,48 +144,12 @@ bool write2iBtn(){
     delay(500);
     return false;
   }
-  byte rwType = getRWtype(); // определяем тип RW-1990.1 или 1990.2
+  byte rwType = getRWtype(); // определяем тип RW-1990.1 или 1990.2 или TM-01
   Serial.print("\n Burning iButton ID: ");
-  switch (rwType) {
-    case 0:                     
-      if (!write2iBtnTM01C()) return false;  //пробуем прошить алгоритмом TM-01C
-      digitalWrite(R_Led, LOW);
-      if (dataIsBurningOK()){
-        Serial.println(" The key TM-01C has copied successesfully");
-        if (keyType == metacom) finalizationTM01cMetacom();
-        if (keyType == cifral) finalizationTM01cCifral(); 
-        Sd_ReadOK();
-        delay(500);
-        digitalWrite(R_Led, HIGH);
-        return true;
-      }
-      Sd_ErrorBeep();
-      digitalWrite(R_Led, HIGH);
-      return false;
-      break;
-    case 1:
-      if (!write2iBtnRW1990_1()) return false;
-      break;
-    case 2:
-      if (!write2iBtnRW1990_2()) return  false;
-      break;
-  }
-  digitalWrite(R_Led, HIGH);
-  if (!dataIsBurningOK()){
-    digitalWrite(R_Led, LOW); 
-    Serial.println(" The copy is faild");
-    Sd_ErrorBeep();
-    digitalWrite(R_Led, HIGH);
-    return false;    
-  }
-  Serial.println(" The key RW1990(1,2) has copied successesfully");
-  Sd_ReadOK();
-  delay(500);
-  return true;
+  return write2iBtnRW1990_1_2_TM01(rwType); //пробуем прошить
 }
 
 void BurnByte(byte data){
-  digitalWrite(iButtonPin, HIGH); pinMode(iButtonPin, OUTPUT);
   for(byte n_bit=0; n_bit<8; n_bit++){ 
     ibutton.write_bit(data & 1);  
     delay(5);                        // даем время на прошивку каждого бита до 10 мс
@@ -226,44 +159,17 @@ void BurnByte(byte data){
 }
 
 bool readiBtn(){
-  int sum4 = 0;
   for (byte i = 0; i < 8; i++) {
     Serial.print(addr[i], HEX); Serial.print(":");
     ReadID[i] = addr[i];                               // копируем прочтенный код в ReadID
-    if (i>3) sum4+=addr[i];                             // считаем сумму последних 4 байт, если там FFFFFFFF, то это не dallas
   }
   Serial.println();
-  if ((addr[0] == 0x01) &&(sum4 < 0x3FC)) {                         // это ключ формата dallas
+  if (addr[0] == 0x01)  {                         // это ключ формата dallas
     keyType = dallas;
-    Serial.println("It`s a dallas");
+    Serial.println("Type: dallas");
     if (OneWire::crc8(addr, 7) != addr[7]) {
       Serial.println("CRC is not valid!");
       Sd_ErrorBeep();
-      digitalWrite(B_Led, HIGH);
-      return false;
-    }
-    Sd_ReadOK();
-    return true;
-  }
-  byte startNibl = addr[0] & B1111;  // выделяем 4 бита
-  if ((sum4 == 0x3FC) && (startNibl == B1110)) {                          //это ключ формата cifral  (addr[3] == 0x01)
-    keyType = cifral;
-    if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      Sd_ErrorBeep();
-      digitalWrite(B_Led, HIGH);
-      return false;
-    }
-    Serial.println("It`s a cifral");
-    Sd_ReadOK();
-    return true;
-  }
-  if ((sum4 == 0x3FC) && (addr[0] == 0x03)) {                          //это ключ формата metacom
-    keyType = metacom;
-    if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      Sd_ErrorBeep();
-      Serial.println("It`s a metacom");
       digitalWrite(B_Led, HIGH);
       return false;
     }
@@ -271,76 +177,75 @@ bool readiBtn(){
     return true;
   }
   keyType = unknown;
+  return true;
 }
 
-bool searchCifral(){
-  //pinMode(iButtonPin, INPUT_PULLUP);
-  digitalWrite(iButtonPin, LOW); pinMode(iButtonPin, OUTPUT);  //отклчаем питание от ключа
-  delay(100);
-  //analogComparator.setOn(0, iButtonPin);  // analogComparator.setOn([AIN+, AIN-]); analogComparator.waitComp([timeout]); INTERNAL_REFERENCE
+unsigned long pulseAComp(bool pulse, unsigned long timeOut = 20000){  // pulse HIGH or LOW
+  bool AcompState;
+  unsigned long tStart = micros();
+  do {
+      AcompState = ACSR & _BV(ACO);  // читаем флаг компаратора
+      if (AcompState == pulse) {
+        tStart = micros();
+        do {
+          AcompState = ACSR & _BV(ACO);  // читаем флаг компаратора
+          if (AcompState != pulse) return (long)(micros() - tStart);  
+        } while ((long)(micros() - tStart) < timeOut);
+        return 0;                                                 //таймаут, импульс не вернуся оратно
+      } // end if
+    } while ((long)(micros() - tStart) < timeOut);
+  return 0;
+}
+
+void ACsetOff(){
+  ACSR |= (1<<ACD); //switch off the AC
+}
+
+void ACsetOn(){
   ADCSRA &= ~(1<<ADEN);      // выключаем ADC
-  AC_REGISTER |= (1<<ACME);   //включаем AC
   ADCSRB |= (1<<ACME);        //включаем AC
   ADMUX = 0b00000101;        // подключаем к AC Линию A5
-  pinMode(iButtonPin, INPUT_PULLUP);
-  
-  for (byte i = 0; i<8; i++){
-    //addr[i] = ibutton.read();//read_bytecifral();  // поиск цифрал
-    addr[i] = read_bytecifral(); //analogRead(iButtonPin) >> 2
-    Serial.print(addr[i], HEX); Serial.print(":");
-    //if (addr[i] == 0xFF) delay(10);  
-  }
-  analogComparator.setOff();
-  Serial.println();
-  /*
-  for (byte i = 0; i < 8; i++) {
-    Serial.print(addr[i], HEX); Serial.print(":");
-    //CheckSumNewKey += ReadID[i];  
-    //if (ReadID[i] == addr[i]) Check++; // сравниваем код для записи с тем, что уже записано в ключе.
-  }
-  */
-  //Sd_ReadOK();
-  delay (1);
-  return false;  
 }
 
-byte read_bytecifral(void){
-  //noInterrupts();
-  long timeMks = 0, ti = 0; byte result = 0;//xFF;
-  //pinMode(A0, INPUT_PULLUP);
-  bool anacompState, preState = 0;
-  for (byte i = 0; i<8; i++){
-    //if (anacompState) bitSet(result, i);
-    //delayMicroseconds(10);
-    timeMks = micros();
-    do {
-      anacompState = ACSR & _BV(ACO);
-      if (anacompState == 1) {
-        ti = micros();
-        do {
-          anacompState = ACSR & _BV(ACO);
-          if (anacompState == 0) {
-            ti = micros() - timeMks; 
-            break;
-          }
-        } while ((long)(micros() - ti) < 210);
-        break;
-      }
-    } while ((long)(micros() - timeMks) < 10000);
-    
-    //if (analogComparator.waitComp(300)) bitClear(result, i);
-    //analogComparator.waitComp(400);
-    //if (analogRead(iButtonPin)>100) bitClear(result, i);
-    timeMks = micros() - timeMks;
-    //timeMks = pulseIn(iButtonPin, LOW, 500);
-    //if (timeMks == 0) continue;
-    //if (timeMks < 15) bitClear(result, i);
-    if ((ti > 20)&&(ti < 60)&&(timeMks < 500)) bitSet(result, i);
-    //if ((timeMks > 50) && (timeMks < 200)) bitSet(result, i);
-    //if (((timeMks < 10) || (timeMks <1000)) && (i >=0 )) return ti;//0xF1;
+bool read_cyfral(byte* buf, byte CyfralPin){
+  unsigned long ti;
+  byte j;
+  digitalWrite(CyfralPin, LOW); pinMode(CyfralPin, OUTPUT);  //отклчаем питание от ключа
+  delay(200);
+  ACsetOn();    //analogComparator.setOn(0, CyfralPin); 
+  pinMode(CyfralPin, INPUT_PULLUP);  // включаем пиание Cyfral
+  for (byte i = 0; i<36; i++){    // чиаем 36 bit
+    ti = pulseAComp(HIGH);
+    if ((ti == 0) || (ti > 200)) break;                      // not Cyfral
+    //if ((ti > 20)&&(ti < 50)) bitClear(buf[i >> 3], 7-j);
+    if ((ti > 50) && (ti < 200)) bitSet(buf[i >> 3], 7-j);
+    j++; if (j>7) j=0; 
   }
-  //interrupts();
-  return result;
+  ACsetOff();
+  if (ti == 0) return false;
+  if ((buf[0] >> 4) != 0b1110) return false;   /// not Cyfral
+  byte test;
+  for (byte i = 1; i<4; i++){
+    test = buf[i] >> 4;
+    if ((test != 1)&&(test != 2)&&(test != 4)&&(test != 8)) return false;
+    test = buf[i] & 0x0F;
+    if ((test != 1)&&(test != 2)&&(test != 4)&&(test != 8)) return false;
+  }
+  return true;
+}
+
+bool searchCyfral(){
+  for (byte i = 0; i < 8; i++) addr[i] = 0;
+  bool rez = read_cyfral(addr, iButtonPin);
+  analogComparator.setOff();
+  if (!rez) return false; 
+  keyType = cyfral;
+  for (byte i = 0; i < 8; i++) {
+    Serial.print(addr[i], HEX); Serial.print(":");
+    ReadID[i] = addr[i];                               // копируем прочтенный код в ReadID
+  }
+  Serial.println("Type: Cyfral ");
+  return true;  
 }
 
 void loop() {
@@ -349,7 +254,7 @@ void loop() {
   if ((BtnPinSt == LOW) &&(preBtnPinSt!= LOW)) BtnClick = true;
     else BtnClick = false;
   preBtnPinSt = BtnPinSt;
-  if ((Serial.read() == 't') || BtnClick) {  // переключаель режима чение/запись
+  if ((Serial.read() == 't') || BtnClick) {  // переключаель режима чтение/запись
     if (readflag == true) {
       writeflag = !writeflag;
       clearLed(); 
@@ -362,69 +267,64 @@ void loop() {
       digitalWrite(B_Led, HIGH);
     }
   }
-  if ((!writeflag)&&searchCifral()) readflag = true;  // запускаем поиск cyfral
-  goto l1;
+  if ((!writeflag) && (searchCyfral())) {  // запускаем поиск cyfral
+    digitalWrite(G_Led, LOW);
+    Sd_ReadOK();
+    readflag = true;
+    clearLed(); digitalWrite(G_Led, HIGH);
+  }
+ // goto l1;
   if (!ibutton.search(addr)) {  // запускаем поиск dallas
     ibutton.reset_search();
     delay(200);
     return;
   }
-  if (!writeflag){
+  if (!writeflag){ 
     digitalWrite(G_Led, LOW);
-    readflag = readiBtn();
+    readflag = readiBtn();       // чиаем ключ dallas
     if (readflag) {
       clearLed(); digitalWrite(G_Led, HIGH);
     }
   }else{
-    if (readflag == true) {
-      write2iBtn();
-      }
-    else {          // сюда испонение не должно попасть
-      clearLed();
-      Sd_ErrorBeep();
-      digitalWrite(B_Led, HIGH);
+    if (readflag == true) write2iBtn();
+      else {          // сюда испонение не должно попасть
+        clearLed();
+        Sd_ErrorBeep();
+        digitalWrite(B_Led, HIGH);
     }
   }
   l1:
-  delay(500);
+  delay(300);
 }
 
 void Sd_ReadOK() {  // звук ОК
-  for (int i=400; i<6000; i=i*1.5) beep(i,20);
+  for (int i=400; i<6000; i=i*1.5) { tone(speakerPin, i); delay(20); }
+  noTone(speakerPin);
 }
 
 void Sd_WriteStep(){  // звук "очередной шаг"
-  for (int i=2500; i<6000; i=i*1.5) beep(i,10);
+  for (int i=2500; i<6000; i=i*1.5) { tone(speakerPin, i); delay(10); }
+  noTone(speakerPin);
 }
 
 void Sd_ErrorBeep() {  // звук "ERROR"
   for (int j=0; j <3; j++){
-    for (int i=1000; i<2000; i=i*1.10) beep(i,10);
+    for (int i=1000; i<2000; i=i*1.1) { tone(speakerPin, i); delay(10); }
     delay(50);
-    for (int i=1000; i>500; i=i*.90) beep(i,10);
+    for (int i=1000; i>500; i=i*1.9) { tone(speakerPin, i); delay(10); }
     delay(50);
   }
+  noTone(speakerPin);
 }
 
 void Sd_StartOK(){   // звук "Успешное включение"
-  beep(note_A7,100); //A 
-  beep(note_G7,100); //G 
-  beep(note_E7,100); //E 
-  beep(note_C7,100); //C
-  beep(note_D7,100); //D 
-  beep(note_B7,100); //B 
-  beep(note_F7,100); //F 
-  beep(note_C8,100); //C
+  tone(speakerPin, NOTE_A7); delay(100);
+  tone(speakerPin, NOTE_G7); delay(100);
+  tone(speakerPin, NOTE_E7); delay(100); 
+  tone(speakerPin, NOTE_C7); delay(100);  
+  tone(speakerPin, NOTE_D7); delay(100); 
+  tone(speakerPin, NOTE_B7); delay(100); 
+  tone(speakerPin, NOTE_F7); delay(100); 
+  tone(speakerPin, NOTE_C7); delay(100);
+  noTone(speakerPin); 
 }
-
-void beep (unsigned long noteFrequency, unsigned long noteDuration)
-{      
-  unsigned int microsecondsPerWave = 1000000/noteFrequency;   // Calculate how many HIGH/LOW cycles there are per millisecond
-  unsigned int loopTime = (unsigned long)(noteDuration * noteFrequency) / 2000; // Play the note for the calculated loopTime.
-  for (int i = 0; i < loopTime; i++)   
-    {   
-      digitalWrite(speakerPin,HIGH); delayMicroseconds(microsecondsPerWave); 
-      digitalWrite(speakerPin,LOW); delayMicroseconds(microsecondsPerWave); 
-    } 
-}
-    
